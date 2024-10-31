b2e8ff68e6f55367da2f92adffb966048afc9318
LocNguyen0 <locmcpe123@gmail.com>
2024-10-31 10:22:55 +0700

    Update logout admin and edit admin interface

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -31,11 +31,16 @@
                 </a>
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="userSettings">
                     <div class="header-profile-actions">
-                        {{-- <a href="#">Profile</a> --}}
-                        <a href="{{ route('admin.accountSetting.index')}}">Settings</a>
-                        <a href="login">Logout</a>
+                        <a href="{{ route('admin.accountSetting.index') }}">Settings</a>
+                        
+                        <!-- Form Logout -->
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                        <a href="{{ url('/') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                     </div>
                 </div>
+                
             </li>
         </ul>
     </div>
