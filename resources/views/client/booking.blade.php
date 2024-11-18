6bbe97733fd7bc9da6d9476814655e783d05993f
Minh Anh <minhbeer2k4@gmail.com>
2024-11-18 22:57:17 +0700

    responsive tạm thời

--- resources/views/client/booking.blade.php
+++ resources/views/client/booking.blade.php
@@ -13,18 +13,17 @@
         <section id="section-book-form">
             <div class="container border">
                 <div class="row text-center">
-                    <div class="col bg-warning p-2 text-uppercase">
+                    <div class="col-12 col-md-6 bg-warning p-2 text-uppercase">
                         <h5>Bước 1 - Chọn giờ ăn</h5>
                     </div>
-                    <div class="col bg-secondary p-2 text-uppercase">
-                        <h5><a href="">Bước 2 -
-                                Thông tin khách hàng</a></h5>
+                    <div class="col-12 col-md-6 bg-secondary p-2 text-uppercase">
+                        <h5><a href="">Bước 2 - Thông tin khách hàng</a></h5>
                     </div>
                 </div>
-                <div class="row d-flex justify-content-center align-items-center">
-                    <div class="row text-center mt-3">
+                <div class="row d-flex justify-content-center align-items-center mt-3">
+                    <div class="row text-center justify-content-center">
                         @foreach ($days as $index => $day)
-                            <a href="javascript:void(0)" class="col border m-1 p-2 day-selector"
+                            <a href="javascript:void(0)" class="col-12 col-md-3 border m-1 p-2 day-selector"
                                 data-index="{{ $index }}">
                                 <div class="fw-bold {{ $index === 0 ? 'text-warning' : 'text-light' }}">
                                     {{ $index === 0 ? 'Hôm nay' : ($index === 1 ? 'Ngày mai' : 'Ngày kia') }}
@@ -40,23 +39,21 @@
                 @foreach ($days as $index => $day)
                     <div class="day-section time-slots" id="day-{{ $index }}"
                         style="{{ $index === 0 ? '' : 'display: none;' }}">
-                        <div class="row container mt-2 d-flex justify-content-center align-items-center">
+                        <div class="row mt-2 d-flex justify-content-center align-items-center row-cols-1 row-cols-md-3">
                             @foreach ($timeSlots as $timeSlot)
                                 @php
-                                    $timeSlotWithDate = Carbon\Carbon::today('Asia/Ho_Chi_Minh')->setTimeFromTimeString(
-                                        $timeSlot,
-                                    );
+                                    $timeSlotWithDate = Carbon\Carbon::today('Asia/Ho_Chi_Minh')->setTimeFromTimeString($timeSlot);
                                 @endphp
 
                                 @if ($day->isToday() && $now->greaterThan($timeSlotWithDate))
-                                    <div class="text-muted col-4 p-2 rounded border d-flex justify-content-center">
-                                        <p class="py-2" style="font-size:30px">{{ $timeSlot }}</p>
+                                    <div class="text-muted col p-2 rounded border d-flex justify-content-center mb-2">
+                                        <p class="py-2 m-0" style="font-size:30px">{{ $timeSlot }}</p>
                                     </div>
                                 @else
-                                    <div class="col-4 p-2 rounded border d-flex justify-content-center time-slot"
+                                    <div class="col p-2 rounded border d-flex justify-content-center time-slot mb-2"
                                         style="cursor: pointer;" data-time="{{ $timeSlot }}"
                                         data-date="{{ $day->format('Y-m-d') }}">
-                                        <p class="text-warning py-2" style="font-size:30px;">{{ $timeSlot }}</p>
+                                        <p class="text-warning py-2 m-0" style="font-size:30px;">{{ $timeSlot }}</p>
                                     </div>
                                 @endif
                             @endforeach
@@ -65,7 +62,7 @@
                 @endforeach
 
                 <div class="text-center my-4">
-                    <button id="confirm-button" class="btn-line">Xác nhận</button>
+                    <button id="confirm-button" class="btn btn-primary">Xác nhận</button>
                 </div>
             </div>
         </section>
